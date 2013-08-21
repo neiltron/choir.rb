e8930046ef08c87c8164e1399bd2ccfbfd259032
Neil Pullman <neil@descend.org>
2013-08-21 19:33:49 -0400

    remove get_stream method

--- lib/choir.rb
+++ lib/choir.rb
@@ -3,35 +3,10 @@ require 'httparty'
 module Choir
   class API
 
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
 
     include HTTParty
-
-    attr_accessor :stream_id, :stream
-
-
-    def initialize ( opts = { } )
-
-      self.class.base_uri    'https://api.choir.io'
-      self.stream_id      =  opts[:stream_id]
-
-      self.stream = []
-
-    end
-
-
-    def get_stream stream_id
-
-      self.stream    = []
-      self.stream_id = stream_id
-
-      resp = self.class.get( "/stream/#{@stream_id}" )
-      raise Error.new( "Wrong Stream ID" ) unless resp.code == 200
-
-      self.stream = resp.parsed_response.to_a[0][1]
-
-    end
-
+    base_uri 'https://api.choir.io'
 
     def post_event (key, data)
 
--- spec/choir_spec.rb
+++ spec/choir_spec.rb
@@ -3,28 +3,11 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 describe Choir::API do
 
   before(:all) do
+
     @api = Choir::API.new
 
-    FakeWeb.register_uri(:get, "https://api.choir.io/stream/mystreamid", :body => File.join(File.dirname(__FILE__), 'fixtures', 'messages.json'), :content_type => "application/json")
-    FakeWeb.register_uri(:get, "https://api.choir.io/stream/wrongstreamid", :status => ["404"])
     FakeWeb.register_uri(:post, "https://api.choir.io/myeventid", :status => ["201"])
 
-  end
-
-  describe "get_stream" do
-
-    it "throws an error if stream not found" do
-
-      expect{ @api.get_stream( 'wrongstreamid' ) }.to raise_error(Choir::API::Error)
-
-    end
-
-    it "populates stream with an array of messages" do
-
-      @api.get_stream( 'mystreamid' )
-      @api.stream.count == 1
-
-    end
   end
 
 
@@ -45,7 +28,6 @@ describe Choir::API do
 
     it "throws an argument error if no key is specified" do
 
-      #this is pretty much the same as the no sound test
       expect{ resp = @api.post_event( {} ) }.to raise_error(ArgumentError)
 
     end
